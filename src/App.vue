062de35adb5982cd24934d1e3d7a48c1df265b79
321638914 <85913672+321638914@users.noreply.github.com>
2023-11-24 11:10:15 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+  import { onLaunch, onShow, onHide } from '@dcloudio/uni-app';
+  onLaunch(() => {
+    console.log('App Launch');
+  });
+  onShow(() => {
+    console.log('App Show');
+  });
+  onHide(() => {
+    console.log('App Hide');
+  });
+</script>
+<style>
+  view,
+  text,
+  image {
+    box-sizing: border-box;
+  }
+
+  image {
+    width: 100%;
+    height: 100%;
+  }
+</style>
